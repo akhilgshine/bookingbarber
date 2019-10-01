98300328c69eacc292e450b39c1184ef3d7e4b3b
akhilgshine <akhilgshine6793@gmail.com>
2019-10-02 11:47:40 +1300

    gallery issues fixed

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.app')
+@section('content')
+
+
+    <div class="row">
+        <div class="container" style="margin-top: 85px; margin-bottom: 85px;">
+
+            <div class="row">
+                <h1 class="text-center">
+                    Our Videos
+                </h1>
+                <div class="text-muted text-center" style="margin-bottom: 35px;">
+                    <i class="fa fa-cut"></i>
+                </div>
+
+                <div class="col-sm-4 text-center">
+
+                    <iframe width="1200" height="440" src="https://www.youtube.com/embed/l_pR5obOwss" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+
+            </div>
+            <div class="row">
+                <h1 class="text-center">
+                    Our Hairstyles
+                </h1>
+                <div class="text-muted text-center" style="margin-bottom: 35px;">
+                    <i class="fa fa-cut"></i>
+                </div>
+
+                <div class="col-sm-4 text-center">
+
+                    <iframe width="1200" height="440" src="https://www.youtube.com/embed/l_pR5obOwss" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
