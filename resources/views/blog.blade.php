cbcbd9a04313190537b1768f245f3cb704614fac
akhilgshine <akhilgshine6793@gmail.com>
2019-10-02 11:46:55 +1300

    blog template added

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,96 @@
+@extends('layout.app')
+@section('content')
+
+        <div class="row" style="margin-top: 50px;">
+            <img src="./Images/banner-massages.jpg" class="img-responsive" />
+        </div>
+
+        <div class="row">
+            <div class="container" style="margin-top: 85px; margin-bottom: 85px;">
+                <div class="row">
+                        <h1 class="text-center">
+                                Blog
+                              </h1> 
+                              <div class="text-muted text-center" style="margin-bottom: 35px;">
+                                <i class="fa fa-cut"></i>
+                              </div>
+                    <!-- blog item -->
+          <div class="col-sm-4 blog-item">
+                <div class="bg-white">
+                  <img src="./Images/bigstock-Barber-cutting-hair-with-sciss-32516945.jpg" class="img-responsive" />
+                  <div class="p-1">
+                      <a href="javascript: void(0);"><h4>Lorem ipsum dolor sit amet</h4></a>
+                      <p><span class="text-muted">July 17, 2019</span> by <span class="text-theme">Admin</span></p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos soluta, dolore harum molestias consectetur.</p>
+                  </div>
+                </div>
+              </div>
+            <!-- blog item END -->
+            <!-- blog item -->
+            <div class="col-sm-4 blog-item">
+                <div class="bg-white">
+                  <img src="./Images/shutterstock_777407209-min.jpg" class="img-responsive" />
+                  <div class="p-1">
+                      <a href="javascript: void(0);"><h4>Lorem ipsum dolor sit amet</h4></a>
+                      <p><span class="text-muted">July 17, 2019</span> by <span class="text-theme">Admin</span></p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos soluta, dolore harum molestias consectetur.</p>
+                  </div>
+                </div>
+              </div>
+            <!-- blog item END -->
+            <!-- blog item -->
+            <div class="col-sm-4 blog-item">
+                <div class="bg-white">
+                  <img src="./Images/GettyImages_92348095.0.jpg" class="img-responsive" />
+                  <div class="p-1">
+                      <a href="javascript: void(0);"><h4>Lorem ipsum dolor sit amet</h4></a>
+                      <p><span class="text-muted">July 17, 2019</span> by <span class="text-theme">Admin</span></p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos soluta, dolore harum molestias consectetur.</p>
+                  </div>
+                </div>
+              </div>
+            <!-- blog item END -->
+
+            <!-- blog item -->
+          <div class="col-sm-4 blog-item">
+                <div class="bg-white">
+                  <img src="./Images/bigstock-Barber-cutting-hair-with-sciss-32516945.jpg" class="img-responsive" />
+                  <div class="p-1">
+                      <a href="javascript: void(0);"><h4>Lorem ipsum dolor sit amet</h4></a>
+                      <p><span class="text-muted">July 17, 2019</span> by <span class="text-theme">Admin</span></p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos soluta, dolore harum molestias consectetur.</p>
+                  </div>
+                </div>
+              </div>
+            <!-- blog item END -->
+            <!-- blog item -->
+            <div class="col-sm-4 blog-item">
+                <div class="bg-white">
+                  <img src="./Images/shutterstock_777407209-min.jpg" class="img-responsive" />
+                  <div class="p-1">
+                      <a href="javascript: void(0);"><h4>Lorem ipsum dolor sit amet</h4></a>
+                      <p><span class="text-muted">July 17, 2019</span> by <span class="text-theme">Admin</span></p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos soluta, dolore harum molestias consectetur.</p>
+                  </div>
+                </div>
+              </div>
+            <!-- blog item END -->
+            <!-- blog item -->
+            <div class="col-sm-4 blog-item">
+                <div class="bg-white">
+                  <img src="./Images/GettyImages_92348095.0.jpg" class="img-responsive" />
+                  <div class="p-1">
+                      <a href="javascript: void(0);"><h4>Lorem ipsum dolor sit amet</h4></a>
+                      <p><span class="text-muted">July 17, 2019</span> by <span class="text-theme">Admin</span></p>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eos soluta, dolore harum molestias consectetur.</p>
+                  </div>
+                </div>
+              </div>
+            <!-- blog item END -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    @endsection
