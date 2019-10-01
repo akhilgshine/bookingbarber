c598856e3da9e8dd88e39c2bd4cb6333355e0768
akhilgshine <akhilgshine6793@gmail.com>
2019-10-01 19:21:00 +1300

    service changes changes

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -2,53 +2,53 @@
 @section('content')
 
 
-    <div class="row">
-        <div class="container" style="margin-top: 85px; margin-bottom: 85px;">
-            <div class="row">
-                <h1 class="text-center">
-                    Gallery
-                </h1>
-                <div class="text-muted text-center" style="margin-bottom: 35px;">
-                    <i class="fa fa-cut"></i>
+        <div class="row">
+            <div class="container" style="margin-top: 85px; margin-bottom: 85px;">
+                <div class="row">
+                        <h1 class="text-center">
+                                Gallery
+                              </h1> 
+                              <div class="text-muted text-center" style="margin-bottom: 35px;">
+                                <i class="fa fa-cut"></i>
+                              </div>
+                    <!-- hair style  -->
+                    <div class="col-sm-4 text-center">
+                        <img src="./Images/shutterstock_777407209-min.jpg" class="img-responsive" />
+                        <h4 class="text-theme">RUGGED STYLE</h4>
+                    </div>
+                    <!-- hair style End -->
+                    <!-- hair style  -->
+                    <div class="col-sm-4 text-center">
+                        <img src="./Images/bigstock-Barber-cutting-hair-with-sciss-32516945.jpg" class="img-responsive" />
+                        <h4 class="text-theme">CLOSE CUT</h4>
+                    </div>
+                    <!-- hair style End -->
+                    <!-- hair style  -->
+                    <div class="col-sm-4 text-center">
+                        <img src="./Images/GettyImages_92348095.0.jpg" class="img-responsive" />
+                        <h4 class="text-theme">SKIN FADE</h4>
+                    </div>
+                    <!-- hair style End -->
+                    <!-- hair style  -->
+                    <div class="col-sm-4 text-center">
+                        <img src="./Images/shutterstock_777407209-min.jpg" class="img-responsive" />
+                        <h4 class="text-theme">RUGGED STYLE</h4>
+                    </div>
+                    <!-- hair style End -->
+                    <!-- hair style  -->
+                    <div class="col-sm-4 text-center">
+                        <img src="./Images/bigstock-Barber-cutting-hair-with-sciss-32516945.jpg" class="img-responsive" />
+                        <h4 class="text-theme">CLOSE CUT</h4>
+                    </div>
+                    <!-- hair style End -->
+                    <!-- hair style  -->
+                    <div class="col-sm-4 text-center">
+                        <img src="./Images/GettyImages_92348095.0.jpg" class="img-responsive" />
+                        <h4 class="text-theme">SKIN FADE</h4>
+                    </div>
+                    <!-- hair style End -->
                 </div>
-                <!-- hair style  -->
-                <div class="col-sm-4 text-center">
-                    <img src="./Images/shutterstock_777407209-min.jpg" class="img-responsive" />
-                    <h4 class="text-theme">RUGGED STYLE</h4>
-                </div>
-                <!-- hair style End -->
-                <!-- hair style  -->
-                <div class="col-sm-4 text-center">
-                    <img src="./Images/bigstock-Barber-cutting-hair-with-sciss-32516945.jpg" class="img-responsive" />
-                    <h4 class="text-theme">CLOSE CUT</h4>
-                </div>
-                <!-- hair style End -->
-                <!-- hair style  -->
-                <div class="col-sm-4 text-center">
-                    <img src="./Images/GettyImages_92348095.0.jpg" class="img-responsive" />
-                    <h4 class="text-theme">SKIN FADE</h4>
-                </div>
-                <!-- hair style End -->
-                <!-- hair style  -->
-                <div class="col-sm-4 text-center">
-                    <img src="./Images/shutterstock_777407209-min.jpg" class="img-responsive" />
-                    <h4 class="text-theme">RUGGED STYLE</h4>
-                </div>
-                <!-- hair style End -->
-                <!-- hair style  -->
-                <div class="col-sm-4 text-center">
-                    <img src="./Images/bigstock-Barber-cutting-hair-with-sciss-32516945.jpg" class="img-responsive" />
-                    <h4 class="text-theme">CLOSE CUT</h4>
-                </div>
-                <!-- hair style End -->
-                <!-- hair style  -->
-                <div class="col-sm-4 text-center">
-                    <img src="./Images/GettyImages_92348095.0.jpg" class="img-responsive" />
-                    <h4 class="text-theme">SKIN FADE</h4>
-                </div>
-                <!-- hair style End -->
             </div>
         </div>
     </div>
-    </div>
-@endsection
+    @endsection
