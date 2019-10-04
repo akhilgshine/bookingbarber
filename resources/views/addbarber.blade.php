057a3b41c0f289736f57ed9467859355a236ca81
ashval20 <ashlyvalsalan1996@gmail.com>
2019-10-04 18:02:05 +1300

    Errors fixed in this page

--- resources/views/addbarber.blade.php
+++ resources/views/addbarber.blade.php
@@ -38,6 +38,9 @@
                 <ul class="nav navtheme navbar-nav navbar-right">
                     <li><a href="{{url('message')}}">Messages</a></li>
                     <li><a href="{{url('addbarber')}}">Add Barber</a></li>
+                    <li><a href="{{url('newblog')}}">Add Blog</a></li>
+                    <li><a href="{{url('/homepage')}}">Logout</a></li>
+                   
 
 
                 </ul>
@@ -54,10 +57,10 @@
                 <i class="fa fa-cut"></i>
             </div>
             <div class="row">
-                @foreach($barbers as $barber )
+                 <!-- @foreach($barbers as $barber )
                     {{$barber}}
 
-                    @endforeach
+                    @endforeach  -->
 
 
                 <div class="col-sm-8">
